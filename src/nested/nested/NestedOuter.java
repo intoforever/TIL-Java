858e9774f9d7aa9f0e87ead4a6c513a6a5f437c9
intoforever <startingfrom33@gmail.com>
2025-04-23 00:00:58 +0900

    #TIL-Java
    - 68강 정적 중첩 클래스
    - 정적중첩클래스와 내부클래스 구분2

--- src/nested/nested/NestedOuter.java
+++ src/nested/nested/NestedOuter.java
@@ -12,7 +12,11 @@ public class NestedOuter {
             System.out.println(nestedInstanceValue);
 
             // static class 이기 때문에 바깥 클래스의 인스턴스 멤버에는 접근할 수 없다.
-            System.out.println(outInstanceValue);
+//            System.out.println(outInstanceValue);
+
+            // 바깥 클래스의 클래스 멤버는 private 이더라도 접근할 수 있다.
+            System.out.println(NestedOuter.outClassValue);
+            System.out.println(outClassValue);
         }
     }
 }
