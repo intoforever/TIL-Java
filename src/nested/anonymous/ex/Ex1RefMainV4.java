02e396df8f4db30915a27a2be07eb52efe0c0738
intoforever <startingfrom33@gmail.com>
2025-05-06 00:07:46 +0900

    #TIL-Java
    - 81강 익명 클래스 활용3
    - 익명클래스로 리팩토링(참조없는 ver)

---
+++ src/nested/anonymous/ex/Ex1RefMainV4.java
@@ -0,0 +1,42 @@
+package nested.anonymous.ex;
+
+import java.util.Random;
+
+/*
+* 81강 익명 클래스 활용3
+* */
+public class Ex1RefMainV4 {
+
+    public static void hello(Process process) {
+        System.out.println("프로그램 시작");
+
+        // 코드 조각 시작
+        process.run();
+        // 코드 조각 종료
+
+        System.out.println("프로그램 종료");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("===== hello 호출 시작! =====");
+
+        // 1. dice
+        hello(new Process() {
+            @Override
+            public void run() {
+                int randomValue = new Random().nextInt(6) + 1;
+                System.out.println("주사위 = " + randomValue);
+            }
+        });
+
+        // 2. sum
+        hello(new Process() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("i = " + i);
+                }
+            }
+        });
+    }
+}
