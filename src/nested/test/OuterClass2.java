de15f8d54e71118eeb8d0f97387ad6312aebe767
intoforever <startingfrom33@gmail.com>
2025-05-06 14:28:23 +0900

    #TIL-Java
    - 82강 문제와 풀이1
    - 문제2 - 내부 클래스를 완성해라

---
+++ src/nested/test/OuterClass2.java
@@ -0,0 +1,13 @@
+package nested.test;
+
+/*
+* 82강 문제와 풀이1
+* 문제2 - 내부 클래스를 완성해라
+* */
+public class OuterClass2 {
+    class InnerClass {
+        public void hello() {
+            System.out.println("InnerClass.hello");
+        }
+    }
+}
