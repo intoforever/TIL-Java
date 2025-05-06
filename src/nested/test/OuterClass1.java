e12e3105fa4854fdc8fcd09e71dd42f0957cac05
intoforever <startingfrom33@gmail.com>
2025-05-06 14:23:43 +0900

    #TIL-Java
    - 82강 문제와 풀이1
    - 문제1 - 정적 중첩 클래스를 완성해라

---
+++ src/nested/test/OuterClass1.java
@@ -0,0 +1,14 @@
+package nested.test;
+
+/*
+* 82강 문제와 풀이1
+* 문제1 - 정적 중첩 클래스를 완성해라
+* */
+public class OuterClass1 {
+
+    public static class NestedClass {
+        public static void hello() {
+            System.out.println("hello");
+        }
+    }
+}
