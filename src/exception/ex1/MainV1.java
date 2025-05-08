6bd09b13ae28bbc70af039a4226b75be3347292d
intoforever <startingfrom33@gmail.com>
2025-05-08 21:28:17 +0900

    #TIL-Java
    - 87강 예외 처리가 필요한 이유3 - 반환 값으로 예외 처리
    - 오류일 때 다음 로직이 호출되지 않도록 예외처리
    - 오류 로그를 남기는 코드 추가

--- src/exception/ex1/MainV1.java
+++ src/exception/ex1/MainV1.java
@@ -7,10 +7,16 @@ import java.util.Scanner;
 * 외부 서버와 통신할 때 발생하는 다양한 문제들을 시뮬레이션 하는 코드 작성
 * - 외부 서버와 연결에 실패한다. (네트워크 오류 등등)
 * - 데이터 전송에 문제가 발생한다.
+*
+* 87강 예외 처리가 필요한 이유3 - 반환 값으로 예외 처리
+* 앞서 발생한 문제를 해결
+* - 연결에 실패하면 데이터를 전송하지 않아야 함
+* - 오류 로그를 남겨야 함
 * */
 public class MainV1 {
     public static void main(String[] args) {
-        NetworkServiceV1_1 networkService = new NetworkServiceV1_1();
+        // NetworkServiceV1_1 networkService = new NetworkServiceV1_1();
+        NetworkServiceV1_2 networkService = new NetworkServiceV1_2();
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("전송할 문자: ");
