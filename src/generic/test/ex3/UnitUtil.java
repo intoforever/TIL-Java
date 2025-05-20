954c5d21b922959d8130e663bb9081ae20bfde41
intoforever <startingfrom33@gmail.com>
2025-05-20 23:16:32 +0900

    #TIL-Java
    - [중급2] 20강 문제와 풀이2
    - 문제 풀이 (영한쌤 ver)

--- src/generic/test/ex3/UnitUtil.java
+++ src/generic/test/ex3/UnitUtil.java
@@ -4,6 +4,10 @@ import generic.test.ex3.unit.BioUnit;
 
 public class UnitUtil {
     public static <T extends BioUnit> T maxHp(T t1, T t2) {
-        return t1.getHp() >= t2.getHp() ? t1 : t2;
+        if (t1.getHp() > t2.getHp()) {
+            return t1;
+        } else {
+            return t2;
+        }
     }
 }
