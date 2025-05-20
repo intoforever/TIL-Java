d31392a48a14fe045b1eac2c6b147735707431f1
intoforever <startingfrom33@gmail.com>
2025-05-20 23:39:41 +0900

    #TIL-Java
    - [중급2] 20강 문제와 풀이2
    - 문제 풀이3 (영한쌤 ver)

--- src/generic/test/ex3/UnitPrinter.java
+++ src/generic/test/ex3/UnitPrinter.java
@@ -1,12 +1,16 @@
 package generic.test.ex3;
 
+import generic.test.ex3.unit.BioUnit;
+
 public class UnitPrinter {
 
-    public static <T extends Shuttle> void printV1(T t) {
-        t.showInfo();
+    public static <T extends BioUnit> void printV1(Shuttle<T> t1) {
+        T unit = t1.out();
+        System.out.println("이름: " + unit.getName() + ", HP: " + unit.getHp());
     }
 
-    public static void printV2(Shuttle<?> unit) {
-        unit.showInfo();
+    public static void printV2(Shuttle<? extends BioUnit> t2) {
+        BioUnit unit = t2.out();
+        System.out.println("이름: " + unit.getName() + ", HP: " + unit.getHp());
     }
 }
