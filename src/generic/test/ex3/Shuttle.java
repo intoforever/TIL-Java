f7ba20cbc70355ccbbf42359c32a8ae2b9569784
intoforever <startingfrom33@gmail.com>
2025-05-20 23:25:02 +0900

    #TIL-Java
    - [중급2] 20강 문제와 풀이2
    - 문제 풀이2 (영한쌤 ver)

--- src/generic/test/ex3/Shuttle.java
+++ src/generic/test/ex3/Shuttle.java
@@ -3,13 +3,17 @@ package generic.test.ex3;
 import generic.test.ex3.unit.BioUnit;
 
 public class Shuttle<T extends BioUnit> {
-    private T t;
+    private T unit;
 
     public void in(T t) {
-        this.t = t;
+        unit = t;
+    }
+
+    public T out() {
+        return unit;
     }
 
     public void showInfo() {
-        System.out.println("이름: " + t.getName() + ", HP: " + t.getHp());
+        System.out.println("이름: " + unit.getName() + ", HP: " + unit.getHp());
     }
 }
