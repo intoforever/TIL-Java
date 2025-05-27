939ecc5eb270b9a85dc5cd038a722c39346695a6
intoforever <startingfrom33@gmail.com>
2025-05-27 21:18:19 +0900

    #TIL-Java
    - [중급2] 37강 직접 구현하는 연결 리스트4 - 제네릭 도입
    - 연결 리스트에 제네릭을 도입하여 타입 안전성 확보

---
+++ src/collection/link/MyLinkedListV3.java
@@ -0,0 +1,134 @@
+package collection.link;
+
+/**
+ * 제네릭을 도입하여 타입 안전성 확보
+ */
+public class MyLinkedListV3<E> {
+    private Node<E> first; // 내부에 중첩클래스로 구현
+    private int size = 0;
+
+    public void add(E e) {
+        Node<E> newNode = new Node<>(e);
+        if (first == null) {
+            first = newNode;
+        } else {
+            Node<E> lastNode = getLastNode();
+            lastNode.next = newNode;
+        }
+        size++;
+    }
+
+    private Node<E> getLastNode() {
+        Node<E> x = first;
+        while (x.next != null) {
+            x = x.next;
+        }
+        return x;
+    }
+
+    // 추가 코드
+    public void add(int index, E e) {
+        Node<E> newNode = new Node<>(e);
+        if (index == 0) {
+            newNode.next = first;
+            first = newNode;
+        } else {
+            Node<E> prev = getNode(index - 1);
+            newNode.next = prev.next;
+            prev.next = newNode;
+        }
+        size++;
+    }
+
+    public E set(int index, E e) {
+        Node<E> x = getNode(index);
+        E oldValue = x.item;
+        x.item = e;
+        return oldValue;
+    }
+
+    // 추가 코드
+    public E remove(int index) {
+        Node<E> removeNode = getNode(index);
+        E removedItem = removeNode.item;
+        if (index == 0) {
+            first = removeNode.next;
+        } else {
+            Node<E> prev = getNode(index - 1);
+            prev.next = removeNode.next;
+        }
+        removeNode.item = null;
+        removeNode.next = null;
+        size--;
+
+        return removedItem;
+    }
+
+    public E get(int index) {
+        Node<E> node = getNode(index);
+        return node.item;
+    }
+
+    private Node<E> getNode(int index) {
+        Node<E> x = first;
+        for (int i = 0; i < index; i++) {
+            x = x.next;
+        }
+        return x;
+    }
+
+    public int indexOf(E e) {
+        int index = 0;
+        for (Node<E> x = first; x != null; x = x.next) {
+            if (e.equals(x.item)) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "MyLinkedListV1{" +
+                "first=" + first +
+                ", size=" + size +
+                '}';
+    }
+
+    /**
+     * Node 클래스는 MyLinkedList 안에서만 사용된다.
+     * 외부에서는 사용할 이유가 없기 때문에 내부에 중첩 클래스로 구현했다.
+     *
+     * ※ 클래스 우선순위
+     * 중첩 클래스를 사용하면 MyLinkedListV3 입장에서 외부에 있는 Node 클래스보다 내부에 선언한 Node 클래스를 먼저 사용
+     */
+    private static class Node<E> {
+        E item; // 현재 노드의 데이터
+        Node<E> next; // 다음 노드의 참조
+
+        public Node(E item) {
+            this.item = item;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            Node<E> x = this;
+            sb.append("[");
+            while (x != null) {
+                sb.append(x.item);
+                if (x.next != null) {
+                    sb.append("->");
+                }
+                x = x.next;
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+    }
+}
