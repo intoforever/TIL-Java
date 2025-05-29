95f6770115160a45438159c3f3b7a9b72fc510a8
intoforever <startingfrom33@gmail.com>
2025-05-30 00:14:51 +0900

    #TIL-Java
    - [중급2] 40강 리스트 추상화2 - 의존관계 주입
    - 의존관계 주입에 대한 이해 (생성자 의존관계 주입)
    - 구현체에 따라 달라지는 성능 비교

--- src/collection/list/BatchProcessor.java
+++ src/collection/list/BatchProcessor.java
@@ -1,21 +1,24 @@
 package collection.list;
 
 /**
- * 추상적인 MyList에 의존하는 BatchProcessor 예시
+ * ※ 의존관계 주입 (Dependency Injection)
+ *   - 다형성과 추상화 활용의 선례
+ *   - BatchProcessor 코드를 전혀 변경하지 않고 리스트 전략(알고리즘)을 MyArrayList에서 MyLinkedList로 변경할 수 있다.
  */
 public class BatchProcessor {
-    // private final MyArrayList<Integer> list = new MyArrayList<>();
-    // private final MyLinkedList<Integer> list = new MyLinkedList<>(); // 코드 변경 필요
-
     private final MyList<Integer> list;
 
-    public BatchProcessor(MyList<Integer> list) { // 생성 시점에 동적으로 변경 가능
+    // 생성자 의존관계 주입
+    public BatchProcessor(MyList<Integer> list) {
         this.list = list;
     }
 
     public void logic(int size) {
-        for (int i = 0; i < size; i++) {
-            list.add(0, i); // 앞에 추가
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) { // O(n)
+            list.add(0, i); // ArrayList: O(n) -> 최종 O(n*n) | LinkedList: O(1) -> 최종 O(n)
         }
+        long endTime = System.currentTimeMillis();
+        System.out.println("크기: " + size + ", 계산 시간: " + (endTime - startTime) + "ms");
     }
 }
