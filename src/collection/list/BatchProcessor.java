4d2d635ec41bdac36ba85fd5104ab68231ba772e
intoforever <startingfrom33@gmail.com>
2025-05-29 23:59:59 +0900

    #TIL-Java
    - [중급2] 40강 리스트 추상화2 - 의존관계 주입
    - 추상적인 인터페이스에 의존하는 버전의 코드

--- src/collection/list/BatchProcessor.java
+++ src/collection/list/BatchProcessor.java
@@ -1,10 +1,17 @@
 package collection.list;
 
 /**
- * 구체적인 MyArrayList에 의존하는 BatchProcessor 예시
+ * 추상적인 MyList에 의존하는 BatchProcessor 예시
  */
 public class BatchProcessor {
-    private final MyArrayList<Integer> list = new MyArrayList<>();
+    // private final MyArrayList<Integer> list = new MyArrayList<>();
+    // private final MyLinkedList<Integer> list = new MyLinkedList<>(); // 코드 변경 필요
+
+    private final MyList<Integer> list;
+
+    public BatchProcessor(MyList<Integer> list) { // 생성 시점에 동적으로 변경 가능
+        this.list = list;
+    }
 
     public void logic(int size) {
         for (int i = 0; i < size; i++) {
