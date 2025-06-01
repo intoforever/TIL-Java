58e2f3a9d90bdbb12582ee00b4aaff03e735f1c4
intoforever <startingfrom33@gmail.com>
2025-06-01 17:13:20 +0900

    #TIL-Java
    - [중급2] 45강 문제와 풀이2
    - 문제 풀이 (영한쌤 ver)

--- src/collection/list/test/ex2/ShoppingCart.java
+++ src/collection/list/test/ex2/ShoppingCart.java
@@ -4,19 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCart {
-    private List<Item> list = new ArrayList<>();
+    private List<Item> items = new ArrayList<>();
 
     public void addItem(Item item) {
-        list.add(item);
+        items.add(item);
     }
 
+    /**
+     * 유지보수성을 가져가는 쪽으로 출력과 합계 분리
+     *
+     * 장바구니 상품 출력
+     * 상품명:마늘, 합계:4000
+     * 상품명:상추, 합계:12000
+     * 전체 가격 합:16000
+     */
     public void displayItems() {
-        int total = 0;
+        // 출력
         System.out.println("장바구니 상품 출력");
-        for (Item item : list) {
-            System.out.printf("상품명:%s, 합계:%d\n", item.getName(), item.getTotalPrice());
-            total += item.getTotalPrice();
+        for (Item item : items) {
+            System.out.println("상품명:" + item.getName() + ", 합계:" + item.getTotalPrice());
         }
-        System.out.println("전체 가격 합: " + total);
+
+        // 합계
+        System.out.println("전체 가격 합:" + calculateTotalPrice());
+    }
+
+    // 합계는 메소드 뽑기 (Ctrl + Alt + M)
+    private int calculateTotalPrice() {
+        int totalPrice = 0;
+        for (Item item : items) {
+            totalPrice += item.getTotalPrice();
+        }
+        return totalPrice;
     }
 }
