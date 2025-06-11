7e00b8051b24c0d6347f2283eae7b759b1a97cbb
intoforever <startingfrom33@gmail.com>
2025-06-11 23:38:26 +0900

    #TIL-Java
    - [중급2] 68강 문제와 풀이1
    - 바로 리스트를 만드는 방법 2가지 소개

--- src/collection/set/test/UniqueNamesTest2.java
+++ src/collection/set/test/UniqueNamesTest2.java
@@ -1,5 +1,6 @@
 package collection.set.test;
 
+import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -15,6 +16,12 @@ import java.util.Set;
 public class UniqueNamesTest2 {
     public static void main(String[] args) {
         Integer[] inputArray = {30, 20, 20, 10, 10};
+        
+        // 리스트를 편하게 만드는 방법
+        // 1. Arrays.asList()는 자바 1.2부터 존재했다.
+        // 2. 자바 9이상을 사용한다면 List.of()를 권장
+        List<Integer> list1 = Arrays.asList(1, 2, 3); // 파라미터의 T... 의미는 배열로 바꿔준다는 뜻
+        List<Integer> list2 = List.of(1, 2, 3);
 
         // 코드 작성
         Set<Integer> set = new LinkedHashSet<>(List.of(inputArray));
