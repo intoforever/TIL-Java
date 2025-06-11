6d0e51f6d96a12fe209cfaf7047b426906965427
intoforever <startingfrom33@gmail.com>
2025-06-11 23:31:52 +0900

    #TIL-Java
    - [중급2] 68강 문제와 풀이1
    - 문제 풀이2 (지현 ver)

---
+++ src/collection/set/test/UniqueNamesTest2.java
@@ -0,0 +1,28 @@
+package collection.set.test;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * 문제와 풀이1
+ * 문제2 - 중복 제거와 입력 순서 유지
+ * - 여러 정수가 입력된다. 여기서 중복 값을 제거하고 값을 출력해라.
+ * - 30, 20, 20, 10, 10이 출력되면 중복을 제거하고 출력되면 된다.
+ * - 단 입력 순서대로 출력해라.
+ *     - 출력 예) 30, 20, 10
+ */
+public class UniqueNamesTest2 {
+    public static void main(String[] args) {
+        Integer[] inputArray = {30, 20, 20, 10, 10};
+
+        // 코드 작성
+        Set<Integer> set = new LinkedHashSet<>();
+        for (Integer i : inputArray) {
+            set.add(i);
+        }
+
+        for (Integer s : set) {
+            System.out.println(s);
+        }
+    }
+}
