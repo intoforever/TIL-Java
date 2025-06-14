3d3725695e5e473754d9a6301de29e9faab8a666
intoforever <startingfrom33@gmail.com>
2025-06-15 00:29:41 +0900

    #TIL-Java
    - [중급2] 79강 문제와 풀이2 - Map2
    - 문제 풀이6 (영한쌤 ver)

--- src/collection/map/test/member/MemberRepository.java
+++ src/collection/map/test/member/MemberRepository.java
@@ -7,25 +7,21 @@ public class MemberRepository {
     private Map<String, Member> memberMap = new HashMap<>();
 
     public void save(Member member) {
-        // 코드 작성
         memberMap.put(member.getId(), member);
     }
 
     public void remove(String id) {
-        // 코드 작성
         memberMap.remove(id);
     }
 
     public Member findById(String id) {
-        // 코드 작성
         return memberMap.get(id);
     }
 
     public Member findByName(String name) {
-        // 코드 작성
-        for (Map.Entry<String, Member> entry : memberMap.entrySet()) {
-            if (entry.getValue().getName().equals(name)) {
-                return memberMap.get(entry.getKey());
+        for (Member member : memberMap.values()) {
+            if (member.getName().equals(name)) {
+                return member;
             }
         }
         return null;
