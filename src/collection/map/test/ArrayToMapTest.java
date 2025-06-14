56c55a30d0f0c055379c63c6c705cae05649801d
intoforever <startingfrom33@gmail.com>
2025-06-14 20:51:02 +0900

    #TIL-Java
    - [중급2] 78강 문제와 풀이1 - Map1
    - 문제 풀이1 (영한쌤 ver)

--- src/collection/map/test/ArrayToMapTest.java
+++ src/collection/map/test/ArrayToMapTest.java
@@ -15,14 +15,14 @@ public class ArrayToMapTest {
         String[][] productArr = {{"Java", "10000"}, {"Spring", "20000"}, {"JPA", "30000"}};
 
         // 주어진 배열로부터 Map 생성 - 코드 작성
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> productMap = new HashMap<>();
         for (String[] product : productArr) {
-            map.put(product[0], Integer.valueOf(product[1]));
+            productMap.put(product[0], Integer.valueOf(product[1]));
         }
 
         // Map의 모든 데이터 출력 - 코드 작성
-        for (String key : map.keySet()) {
-            System.out.println("제품: " + key + ", 가격: " + map.get(key));
+        for (String key : productMap.keySet()) {
+            System.out.println("제품: " + key + ", 가격: " + productMap.get(key));
         }
     }
 }
