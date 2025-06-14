e7d875f6d0bd0d741e5995c54d07e63f70cbbbd6
intoforever <startingfrom33@gmail.com>
2025-06-15 00:12:54 +0900

    #TIL-Java
    - [중급2] 79강 문제와 풀이2 - Map2
    - 문제 풀이5 (영한쌤 ver)

--- src/collection/map/test/DictionaryTest.java
+++ src/collection/map/test/DictionaryTest.java
@@ -15,39 +15,39 @@ import java.util.Scanner;
 public class DictionaryTest {
     public static void main(String[] args) {
         // 코드 작성
-        Scanner scanner = new Scanner(System.in);
         Map<String, String> dictionary = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("==단어 입력 단계==");
         while (true) {
             System.out.print("영어 단어를 입력하세요 (종료는 'q'): ");
-            String engWord = scanner.nextLine();
+            String englishWord = scanner.nextLine();
 
-            if ("q".equals(engWord)) {
+            if ("q".equals(englishWord)) {
                 break;
             }
 
             System.out.print("한글 뜻을 입력하세요: ");
-            String korWord = scanner.nextLine();
+            String koreanMeaning = scanner.nextLine();
 
-            dictionary.put(engWord, korWord);
+            dictionary.put(englishWord, koreanMeaning);
         }
 
         System.out.println("==단어 검색 단계==");
         while (true) {
             System.out.print("찾을 영어 단어를 입력하세요 (종료는 'q'): ");
-            String findWord = scanner.nextLine();
+            String searchWord = scanner.nextLine();
 
-            if ("q".equals(findWord)) {
+            if ("q".equals(searchWord)) {
                 break;
             }
 
-            if (!dictionary.containsKey(findWord)) {
-                System.out.println(findWord + "은(는) 사전에 없는 단어입니다.");
-                continue;
+            if (dictionary.containsKey(searchWord)) {
+                String koreanMeaning = dictionary.get(searchWord);
+                System.out.println(searchWord + "의 뜻: " + koreanMeaning);
+            } else {
+                System.out.println(searchWord + "은(는) 사전에 없는 단어입니다.");
             }
-
-            System.out.println(findWord + "의 뜻: " + dictionary.get(findWord));
         }
     }
 }
