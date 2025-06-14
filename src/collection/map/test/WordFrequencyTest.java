0f338a3fc233a060bbe716e6d6aef1ffca8cb3d9
intoforever <startingfrom33@gmail.com>
2025-06-14 21:11:02 +0900

    #TIL-Java
    - [중급2] 78강 문제와 풀이1 - Map1
    - 문제 풀이3 (지현 ver)

---
+++ src/collection/map/test/WordFrequencyTest.java
@@ -0,0 +1,25 @@
+package collection.map.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 문제3 - 같은 단어가 나타난 수
+ * - 각각의 단어가 나타난 수를 출력해라.
+ * - 실행 결과를 참고하자.
+ */
+public class WordFrequencyTest {
+    public static void main(String[] args) {
+        String text = "orange banana apple apple banana apple";
+        
+        // 코드 작성
+        String[] strArr = text.split(" ");
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String str : strArr) {
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+
+        System.out.println(map);
+    }
+}
