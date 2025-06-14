7e8686c3f4f0c8996c02ff1b8c92ef7c1a8ee155
intoforever <startingfrom33@gmail.com>
2025-06-14 21:19:01 +0900

    #TIL-Java
    - [중급2] 78강 문제와 풀이1 - Map1
    - 문제 풀이4 (지현 ver)

---
+++ src/collection/map/test/ItemPriceTest.java
@@ -0,0 +1,30 @@
+package collection.map.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 문제4 - 값으로 검색
+ * - 다음 예제에서 Map에 들어있는 데이터 중에 값이 1000원인 모든 상품을 출력해라.
+ * - 실행 결과를 참고하자.
+ */
+public class ItemPriceTest {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("사과", 500);
+        map.put("바나나", 500);
+        map.put("망고", 1000);
+        map.put("딸기", 1000);
+
+        // 코드 작성
+        List<String> list = new ArrayList<>();
+        for (String key : map.keySet()) {
+            if (map.get(key) == 1000) {
+                list.add(key);
+            }
+        }
+        System.out.println(list);
+    }
+}
