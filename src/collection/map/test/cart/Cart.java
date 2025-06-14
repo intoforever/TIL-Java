23378c8888fb107da59644fcd128deef49fce8d2
intoforever <startingfrom33@gmail.com>
2025-06-15 00:54:42 +0900

    #TIL-Java
    - [중급2] 79강 문제와 풀이2 - Map2
    - 문제 풀이7 (영한쌤 ver)

--- src/collection/map/test/cart/Cart.java
+++ src/collection/map/test/cart/Cart.java
@@ -7,22 +7,25 @@ public class Cart {
     private Map<Product, Integer> cartMap = new HashMap<>();
     
     // 코드 작성
-    public void add(Product product, int amount) {
-        cartMap.put(product, cartMap.getOrDefault(product, 0) + amount);
+    public void add(Product product, int addQuantity) {
+        Integer existingQuantity = cartMap.getOrDefault(product, 0);
+        cartMap.put(product, existingQuantity + addQuantity);
     }
 
-    public void minus(Product product, int amount) {
-        if (cartMap.get(product) <= amount) {
+    public void minus(Product product, int quantity) {
+        int existingQuantity = cartMap.getOrDefault(product, 0);
+        int newQuantity = existingQuantity - quantity;
+        if (newQuantity <= 0) {
             cartMap.remove(product);
-            return;
+        } else {
+            cartMap.put(product, newQuantity);
         }
-        cartMap.put(product, cartMap.get(product) - amount);
     }
 
     public void printAll() {
         System.out.println("==모든 상품 출력==");
-        for (Product product : cartMap.keySet()) {
-            System.out.println("상품: " + product + " 수량: " + cartMap.get(product));
+        for (Map.Entry<Product, Integer> entry : cartMap.entrySet()) {
+            System.out.println("상품: " + entry.getKey() + " 수량: " + entry.getValue());
         }
     }
 }
