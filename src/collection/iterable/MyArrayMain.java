a02269442a7845a377a64169e0f7f69c335e94d8
intoforever <startingfrom33@gmail.com>
2025-06-17 20:38:19 +0900

    #TIL-Java
    - [중급2] 84강 순회2 - 향상된 for문
    - Iterable을 구현한 자료구조는 향상된 for문을 사용할 수 있다.

--- src/collection/iterable/MyArrayMain.java
+++ src/collection/iterable/MyArrayMain.java
@@ -15,5 +15,27 @@ public class MyArrayMain {
             Integer value = iterator.next();
             System.out.println("value = " + value);
         }
+
+        // 추가
+        System.out.println("for-each 사용");
+        for (Integer value : myArray) {
+            System.out.println("value = " + value);
+        }
     }
 }
+
+/**
+ * for-each문으로 불리는 향상된 for문은 자료 구조를 순회하는 것이 목적이다.
+ * 자바는 Iterable 인터페이스를 구현한 객체에 대해서 향상된 for문을 사용할 수 있게 해준다.
+ *
+ * 자바는 아래의 for-each문을
+ * for (int value : myArray) {
+ *     System.out.println("value = " + value);
+ * }
+ *
+ * 컴파일 시점에 다음과 같이 변경한다.
+ * while (iterator.hasNext()) {
+ *     Integer value = iterator.next();
+ *     System.out.println("value = " + value);
+ * }
+ */
